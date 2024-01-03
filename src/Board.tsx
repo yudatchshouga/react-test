7ebfeaf0eed2e95c76393c29f0776b6b3509ddb5
yudatchshouga <>
2024-01-03 18:47:54 +0900

    右にある駒を返す機能を実装

--- src/Board.tsx
+++ src/Board.tsx
@@ -20,11 +20,48 @@ const Board: React.FC<BoardProps> = () => {
 
   const [board, setBoard] = useState<CellState[][]>(initializeBoard);
 
+  const flipPiecesRight = (
+    board: CellState[][],
+    rowIndex: number,
+    cellIndex: number,
+    currentPlayer: CellState
+  ) => {
+    let canFlip = false;
+    const positionsToFlip = [];
+    // 右方向にあるコマをチェック
+    for (let c = cellIndex + 1; c < board[rowIndex].length; c++) {
+      if (board[rowIndex][c] === CellState.Empty) {
+        // 空のセルに遭遇したら終了
+        break;
+      } else if (board[rowIndex][c] !== currentPlayer) {
+        // 異なる色のコマに遭遇したら、位置を記録
+        positionsToFlip.push([rowIndex, c]);
+      } else {
+        // 同じ色のコマに遭遇したら、ひっくり返すことができる
+        canFlip = true;
+        break;
+      }
+    }
+    // ひっくり返すことができる場合、コマをひっくり返す
+    if (canFlip) {
+      positionsToFlip.forEach(([r, c]) => {
+        board[r][c] = currentPlayer;
+      });
+    }
+    return board;
+  };
+
   const handleCellClick = (rowIndex: number, cellIndex: number) => {
     setBoard((prevBoard) => {
       if (prevBoard[rowIndex][cellIndex] === CellState.Empty) {
-        const newBoard = prevBoard.map((row) => [...row]);
+        let newBoard = prevBoard.map((row) => [...row]);
         newBoard[rowIndex][cellIndex] = CellState.Black;
+        newBoard = flipPiecesRight(
+          newBoard,
+          rowIndex,
+          cellIndex,
+          CellState.Black
+        );
         return newBoard;
       }
       return prevBoard;
