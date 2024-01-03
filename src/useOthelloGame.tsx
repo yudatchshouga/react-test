6a9657bb1fe489ac5727658671646a7dd923a59d
yudatchshouga <>
2024-01-03 23:44:10 +0900

    右側だけ置けるかどうかの判定が可能になった

--- src/useOthelloGame.tsx
+++ src/useOthelloGame.tsx
@@ -16,12 +16,53 @@ const initializeBoard = (): CellModel[][] => {
   board[4][3].piece = PieceModel.Black;
   board[4][4].piece = PieceModel.White;
 
-  board[2][3].canPut = true;
-
-  console.log(board);
+  checkPutableCells(board, PieceModel.Black);
 
   return board;
 };
+
+function checkPutableCells(board: CellModel[][], currentPlayer: PieceModel) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      checkPutableCell(board, currentPlayer, i, j);
+    }
+  }
+}
+
+function checkPutableCell(
+  board: CellModel[][],
+  currentPlayer: PieceModel,
+  rowIndex: number,
+  cellIndex: number
+) {
+  let cell = board[rowIndex][cellIndex];
+  // セルが空でない場合、そこにはコマを置けない
+  if (cell.piece !== PieceModel.None) {
+    cell.canPut = false;
+    return;
+  }
+
+  // 右方向をチェック
+  let foundOpponent = false;
+  for (let j = cellIndex + 1; j < board[rowIndex].length; j++) {
+    if (board[rowIndex][j].piece === getOpponent(currentPlayer)) {
+      foundOpponent = true;
+    } else if (board[rowIndex][j].piece === currentPlayer && foundOpponent) {
+      // 対戦相手のコマの後に現在のプレイヤーのコマがある
+      cell.canPut = true;
+      return;
+    } else {
+      break;
+    }
+  }
+
+  cell.canPut = false;
+  return;
+}
+
+function getOpponent(player: PieceModel): PieceModel {
+  return player === PieceModel.Black ? PieceModel.White : PieceModel.Black;
+}
 
 export const useOthelloGame = () => {
   const [board, setBoard] = useState<CellModel[][]>(initializeBoard);
