77681e3be77f2f8767be146c8e8bf0ed4eff9147
yudatchshouga <>
2024-01-04 11:54:35 +0900

    ロジックをボードに集約

--- src/models/BoardModel.ts
+++ src/models/BoardModel.ts
@@ -1,4 +1,6 @@
+import { DIRECTIONS } from "../constants/Directions";
 import CellModel from "./CellModel";
+import Direction from "./Direction";
 import PieceModel from "./PieceModel";
 import Position from "./Position";
 
@@ -20,6 +22,8 @@ class BoardModel {
     this.getCell(new Position(size / 2 - 1, size / 2)).piece = PieceModel.White;
     this.getCell(new Position(size / 2, size / 2 - 1)).piece = PieceModel.White;
     this.getCell(new Position(size / 2, size / 2)).piece = PieceModel.Black;
+
+    this.setCanPutToBoard(PieceModel.Black);
   }
 
   getCells(): CellModel[][] {
@@ -59,6 +63,82 @@ class BoardModel {
     flippablePositions.map((position) => {
       this.getCell(position).piece = currentPlayer;
     });
+  }
+
+  setCanPutToBoard(currentPlayer: PieceModel) {
+    for (let y = 0; y < this.size; y++) {
+      for (let x = 0; x < this.size; x++) {
+        let position = new Position(x, y);
+        this.setCanPutToCell(currentPlayer, position);
+      }
+    }
+  }
+
+  setCanPutToCell(currentPlayer: PieceModel, position: Position) {
+    let cell = this.getCell(position);
+    let allFlippablePositions: Position[] = [];
+    let canPut = false;
+
+    DIRECTIONS.forEach((direction) => {
+      let flippablePositions = this.canPutToDirection(
+        currentPlayer,
+        position,
+        direction
+      );
+
+      if (flippablePositions.length > 0) {
+        allFlippablePositions.push(...flippablePositions);
+        canPut = true; // 少なくとも一つの方向でコマを置くことができる
+      }
+    });
+
+    cell.setCanPut(canPut);
+    if (canPut) {
+      cell.setFlippablePositions(allFlippablePositions);
+      console.log(allFlippablePositions);
+    }
+  }
+
+  canPutToDirection = (
+    currentPlayer: PieceModel,
+    position: Position,
+    direction: Direction
+  ): Position[] => {
+    let cell = this.getCell(position);
+    let flippablePositions: Position[] = [];
+    if (cell.piece === PieceModel.None) {
+      let foundOpponent = false;
+      position = position.addDirection(direction);
+      while (position.isVaild(this.size)) {
+        // 盤面の範囲内でループを続ける
+        // xとyは、現在調査中のセルの座標
+        if (this.getCell(position).piece === this.getOpponent(currentPlayer)) {
+          // 現在のプレイヤーの対戦相手のコマが見つかった場合
+          // 例: 現在のプレイヤーが黒の場合、白のコマを見つけた状況
+          foundOpponent = true;
+          flippablePositions.push(position);
+        } else if (
+          this.getCell(position).piece === currentPlayer &&
+          foundOpponent
+        ) {
+          // 現在のプレイヤーのコマが見つかり、かつ対戦相手のコマを既に見つけていた場合
+          // これはコマを置くことができる状況を意味する
+          return flippablePositions;
+        } else {
+          // いずれの条件も満たさない場合、ループを終了
+          // 例: 空のセルに遭遇したり、盤面の端に達した場合
+          break;
+        }
+        // 次のセルへ移動
+        position = position.addDirection(direction);
+      }
+    }
+
+    return [];
+  };
+
+  getOpponent(player: PieceModel): PieceModel {
+    return player === PieceModel.Black ? PieceModel.White : PieceModel.Black;
   }
 }
 
--- src/useOthelloGame.tsx
+++ src/useOthelloGame.tsx
@@ -2,99 +2,9 @@ import { useState } from "react";
 import PieceModel from "./models/PieceModel";
 import Position from "./models/Position";
 import BoardModel from "./models/BoardModel";
-import { DIRECTIONS } from "./constants/Directions";
-import Direction from "./models/Direction";
-
-const initializeBoard = (): BoardModel => {
-  const board = new BoardModel(8);
-  setCanPutToBoard(board, PieceModel.Black);
-  return board;
-};
-
-const setCanPutToBoard = (board: BoardModel, currentPlayer: PieceModel) => {
-  for (let y = 0; y < board.size; y++) {
-    for (let x = 0; x < board.size; x++) {
-      let position = new Position(x, y);
-      setCanPutToCell(board, currentPlayer, position);
-    }
-  }
-};
-
-const setCanPutToCell = (
-  board: BoardModel,
-  currentPlayer: PieceModel,
-  position: Position
-) => {
-  let cell = board.getCell(position);
-  let allFlippablePositions: Position[] = [];
-  let canPut = false;
-
-  DIRECTIONS.forEach((direction) => {
-    let flippablePositions = canPutToDirection(
-      board,
-      currentPlayer,
-      position,
-      direction
-    );
-
-    if (flippablePositions.length > 0) {
-      allFlippablePositions.push(...flippablePositions);
-      canPut = true; // 少なくとも一つの方向でコマを置くことができる
-    }
-  });
-
-  cell.setCanPut(canPut);
-  if (canPut) {
-    cell.setFlippablePositions(allFlippablePositions);
-    console.log(allFlippablePositions);
-  }
-};
-
-const canPutToDirection = (
-  board: BoardModel,
-  currentPlayer: PieceModel,
-  position: Position,
-  direction: Direction
-): Position[] => {
-  let cell = board.getCell(position);
-  let flippablePositions: Position[] = [];
-  if (cell.piece === PieceModel.None) {
-    let foundOpponent = false;
-    position = position.addDirection(direction);
-    while (position.isVaild(board.size)) {
-      // 盤面の範囲内でループを続ける
-      // xとyは、現在調査中のセルの座標
-      if (board.getCell(position).piece === getOpponent(currentPlayer)) {
-        // 現在のプレイヤーの対戦相手のコマが見つかった場合
-        // 例: 現在のプレイヤーが黒の場合、白のコマを見つけた状況
-        foundOpponent = true;
-        flippablePositions.push(position);
-      } else if (
-        board.getCell(position).piece === currentPlayer &&
-        foundOpponent
-      ) {
-        // 現在のプレイヤーのコマが見つかり、かつ対戦相手のコマを既に見つけていた場合
-        // これはコマを置くことができる状況を意味する
-        return flippablePositions;
-      } else {
-        // いずれの条件も満たさない場合、ループを終了
-        // 例: 空のセルに遭遇したり、盤面の端に達した場合
-        break;
-      }
-      // 次のセルへ移動
-      position = position.addDirection(direction);
-    }
-  }
-
-  return [];
-};
-
-const getOpponent = (player: PieceModel): PieceModel => {
-  return player === PieceModel.Black ? PieceModel.White : PieceModel.Black;
-};
 
 export const useOthelloGame = () => {
-  const [board, setBoard] = useState<BoardModel>(initializeBoard);
+  const [board, setBoard] = useState<BoardModel>(new BoardModel(8));
   const [currentPlayer, setCurrentPlayer] = useState<PieceModel>(
     PieceModel.Black
   );
@@ -105,8 +15,8 @@ export const useOthelloGame = () => {
       if (selectedCell.getCanPut()) {
         let newBoard = prevBoard.copy();
         newBoard.putPiece(position, currentPlayer);
-        setCanPutToBoard(newBoard, getOpponent(currentPlayer));
-        setCurrentPlayer(getOpponent(currentPlayer));
+        newBoard.setCanPutToBoard(newBoard.getOpponent(currentPlayer));
+        setCurrentPlayer(newBoard.getOpponent(currentPlayer));
         console.log(newBoard.cells);
         return newBoard;
       }
