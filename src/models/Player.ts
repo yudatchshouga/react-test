49a627b786ee4c22bfcae97d704b10181f7890ae
yudatchshouga <>
2024-01-04 12:35:13 +0900

    player作成

---
+++ src/models/Player.ts
@@ -0,0 +1,13 @@
+import PieceModel from "./PieceModel";
+
+class Player {
+    name: string;
+    piece: PieceModel;
+    canPut: boolean = true;
+    constructor(name: string, piece: PieceModel) {
+        this.name = name;
+        this.piece = piece;
+    }
+}
+
+export default Player;
