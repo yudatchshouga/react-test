d8879726fdeeb719fddeadd09f2cbfa4f41b5a19
yudatchshouga <>
2024-01-04 18:56:15 +0900

    canPutがfalseになるようになった

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,16 +1,46 @@
 import Board from "./Board";
 import PieceModel from "../models/PieceModel";
-import { useOthelloGame } from "../hooks/useOthelloGame";
+import Player from "../models/Player";
+import GameModel from "../models/GameModel";
+import { useState } from "react";
+import Position from "../models/Position";
+import Players from "../models/Players";
 
 type GameProps = {};
 
 const Game: React.FC<GameProps> = () => {
-  const { game, currentPlayer, onClickCell } = useOthelloGame();
+  const players: Players = new Players([
+    new Player("Player1", PieceModel.Black),
+    new Player("Player2", PieceModel.White),
+  ]);
+
+  const startPlayer = players.getStartPlayer();
+
+  const [game, setGame] = useState<GameModel>(new GameModel(8, startPlayer));
+
+  const onClickCell = (position: Position) => {
+    setGame((prevGame) => {
+      if (prevGame.board.getCell(position).getCanPut()) {
+        let game = prevGame.copy();
+        const currentPlayer = game.currentPlayer;
+        game.putPiece(position, currentPlayer.piece);
+
+        const nextPlayer = players.getNextPlayer(currentPlayer);
+        game.currentPlayer = nextPlayer;
+        game.board.setCanPutToBoard(nextPlayer);
+        game.board.setCanPutToPlayer(nextPlayer);
+
+        return game;
+      }
+      return prevGame;
+    });
+  };
 
   return (
     <div>
       <p>
-        Current Player: {currentPlayer === PieceModel.Black ? "Black" : "White"}
+        Current Player: {game.currentPlayer.name}, canPut:
+        {game.currentPlayer.canPut.toString()}
       </p>
       <Board board={game.board} onClickCell={onClickCell} />
     </div>
