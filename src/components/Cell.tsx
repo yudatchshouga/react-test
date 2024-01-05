3c0bd845f03a79508b3fe52788100b681123942b
yudatchshouga <>
2024-01-05 13:32:39 +0900

    保存

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,30 +1,21 @@
-import styled from "styled-components";
+type CellProps = {
+  content: string;
+};
 
-interface CellProps {
-  cell: string;
-  onClick: () => void;
-}
-
-const cellSize: number = 70;
-const defaultColor: string = "#29a83f";
-const canPutColor: string = "lightgreen";
-
-const SCell = styled.div<{ color: string }>`
-  width: ${cellSize}px;
-  height: ${cellSize}px;
-  background-color: ${(props) => props.color};
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Cell: React.FC<CellProps> = ({ cell, onClick }) => {
-  const color = cell === "None" ? canPutColor : defaultColor;
+const Cell: React.FC<CellProps> = (props) => {
   return (
-    <SCell onClick={onClick} color={color}>
-      {cell}
-    </SCell>
+    <div
+      style={{
+        width: "50px",
+        height: "50px",
+        border: "1px solid black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {props.content}
+    </div>
   );
 };
 
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,60 +1,17 @@
-import Board from "./Board";
-import GameModel from "../models/GameModel";
-import { useEffect, useState } from "react";
-import Position from "../models/Position";
+import Cell from "./Cell";
 
 type GameProps = {};
 
 const Game: React.FC<GameProps> = () => {
-  const [game, setGame] = useState<GameModel>(new GameModel());
-
-  useEffect(() => {
-    if (!game.currentPlayer.canPut) {
-      const timer = setTimeout(() => {
-        setGame((prevGame) => {
-          let _game = prevGame.copy();
-          _game.toNextPlayer();
-          _game.setCanPut();
-          _game.setIsOver();
-          return _game;
-        });
-      }, 1000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [game.currentPlayer.canPut]);
-
-  const onClickCell = (position: Position) => {
-    setGame((game) => {
-      // positionに駒を置けるかどうか
-      if (game.canPut(position)) {
-        // gameのコピーを作成
-        let _game = game.copy();
-        // console.log("copy done");
-        // 駒を置いて、相手の駒をひっくり返す
-        _game.putPiece(position);
-        console.log("put done");
-        // 次のプレイヤーに交代
-        _game.toNextPlayer();
-        // console.log("next done");
-        // 駒を置けるかどうかのフラグを立てる
-        _game.setCanPut();
-        // console.log("canPut done");
-        return _game;
-      }
-      return game;
-    });
-  };
-
   return (
     <div>
-      <p>
-        Current Player: {game.currentPlayer.name}, canPut:
-        {game.currentPlayer.canPut.toString()}
-      </p>
-      {!game.currentPlayer.canPut && <p>PASS</p>}
-      {game.isOver && <p>GAME OVER</p>}
-      <Board board={game.board} onClickCell={onClickCell} />
+      {[...Array(4)].map((_, i) => (
+        <div style={{ display: "flex" }} key={i}>
+          {[...Array(4)].map((_, j) => (
+            <Cell key={j} content="⚫︎" />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
